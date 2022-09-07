788f9d74a271199071a3a8b4fb7e30b1a6f8cb8d
Ojoge Moses <ojogemoses1@gmail.com>
2022-09-07 21:25:32 +0100

    audiophile e-commerce website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react"
+import SharedLayout from "./SharedLayout"
+import Home from "./components/Home/Home"
+import Category from "./components/Category/Category"
+import Product from "./components/Product/Product"
+import Cart from "./components/Cart/Cart"
+import Checkout from "./components/Checkout/Checkout"
+import { Routes, Route } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { addCartTotal } from "./features/CartReducer"
+
+
+/*let goat = <Route path = "/category/:category" element = {<Category />} /> */
 
 function App() {
+  let { cartItems } = useSelector((store) => store.cart)
+  console.log(cartItems)
+    //let { amount } = useSelector(store => store.cart)
+    
+    let dispatch = useDispatch()
+    
+    useEffect(() => {
+      dispatch(addCartTotal())
+    }, [cartItems])
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Cart />
+    <Routes>
+      <Route path = "/" element = {<SharedLayout />}>
+        <Route index element = {<Home />}/>
+        <Route path = "/checkout" element = {<Checkout cartItem = {cartItems}/>}/>
+        <Route path = "/category/:category" element = {<Category />} />
+        <Route path = "/product/:product" element = {<Product />} />
+      </Route>
+    </Routes>
+    </main>
   );
 }
 
