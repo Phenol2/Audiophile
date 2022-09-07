77ca84160ae910c762e5e2ce78ccae0ff174210e
Ojoge Moses <ojogemoses1@gmail.com>
2022-09-08 00:16:15 +0100

    index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { StoreProvider } from "./store/Storectx"
 import { store } from "./reduxStore/store"
 import { Provider } from "react-redux"
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StoreProvider>
   <React.StrictMode>
-   <BrowserRouter>
+   <HashRouter>
     <Provider store = {store}>
      <App />
      </Provider>
-   </BrowserRouter>
+   </HashRouter>
   </React.StrictMode>
   </StoreProvider>
 );
